1d1118dd5ebbf5c1bd612ce1e5a46fd665dad6de
dtsirkov <dtsirkov@gmail.com>
2014-06-04 00:37:16 +0300

    V2

---
+++ src/property_pckg/ManageProperty.java
@@ -0,0 +1,35 @@
+package property_pckg;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ManageProperty {
+
+	public static final String getExceptionDtl(String propertyName){
+		return ManageProperty.getProperty(propertyName, "exception_dtl.properties");
+	}
+	
+	public static final String getLabelDtl(String propertyName){
+		return ManageProperty.getProperty(propertyName, "label.properties");
+	}
+
+	public static final String getProperty(String propertyName, String fileName)
+	{
+		Properties prop = new Properties();
+		String propertyValue = "";
+		try {
+			//load a properties file from class path, inside static method
+			InputStream in = ManageProperty.class.getResourceAsStream(fileName);
+			prop.load(in);
+			propertyValue = prop.getProperty(propertyName);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		return propertyValue;
+	}
+
+	public static void main(String[] args){
+		System.out.println(ManageProperty.getExceptionDtl("IncorrectSocialNumber_en"));
+	}
+
+}
--- src/web/CustomValidator.java
+++ src/web/CustomValidator.java
@@ -3,18 +3,30 @@ package web;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import property_pckg.ManageProperty;
+
 import com.vaadin.data.Validator;
 
-class CustomValidator {
+class CustomValidator implements java.io.Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2270732896484883778L;
+	private String language;
+
+	public CustomValidator(String language){
+		this.setLanguage(language);
+	}
 
 	//vslidate social number
-	public static final Validator socialNumber(){
+	public final Validator getOnlyDigitsValidator(final String propertyName){
 		class MyValidator implements Validator {
 			private static final long serialVersionUID = -8281962473854901819L;
 			@Override
-			public void validate(Object value) throws InvalidValueException {
+			public void validate(Object value) throws InvalidValueException {				
 				if (!isValid(value))
-					throw new InvalidValueException("Incorrect input!");
+					throw new InvalidValueException(ManageProperty.getExceptionDtl(propertyName));
 			}
 
 			public boolean isValid(Object value) {
@@ -24,12 +36,20 @@ class CustomValidator {
 				Pattern pattern = Pattern.compile(expression);  
 				Matcher matcher = pattern.matcher(inputStr);  
 				if(matcher.matches()){  
-				isValid = true;  
+					isValid = true;  
 				}  
 				return isValid;  
-				}
 			}
+		}
 		return new MyValidator();
 	}
-	
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
 }
